e57a815fcbca06b8d882694bd20b10580cd8edc0
sadhanaM66 <reddysadhana150@gmail.com>
2026-07-12 08:42:51 +0530

    updated

--- tests/test1.spec.js
+++ tests/test1.spec.js
@@ -6,5 +6,6 @@ test("login", async({page})=>{
     await page.locator('//input[@id="password"]').fill("secret_sauce")
     await page.locator('//input[@id="login-button"]').click()
     //modified
+    //TM1
 }
 )
