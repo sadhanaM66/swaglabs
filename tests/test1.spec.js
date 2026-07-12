f9f980681604a355db71bb9228905029789c93e8
sadhanaM66 <reddysadhana150@gmail.com>
2026-07-12 08:33:48 +0530

    testing

---
+++ tests/test1.spec.js
@@ -0,0 +1,10 @@
+import {test} from "@playwright/test"
+
+test("login", async({page})=>{
+    await page.goto('https://www.saucedemo.com/')
+    await page.locator('//input[@id="user-name"]').fill("standard_user")
+    await page.locator('//input[@id="password"]').fill("secret_sauce")
+    await page.locator('//input[@id="login-button"]').click()
+    //modified
+}
+)
